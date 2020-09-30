1a8fd686021737d137699138ab7c8357658ea235
nil2425 <nil2425@gmail.com>
2020-09-30 19:42:34 +0530

    Assignment 3 - Plugin development updates

---
+++ app/code/Assignment3/PluginDevelopment/Plugin/Cart.php
@@ -0,0 +1,21 @@
+<?php
+ 
+namespace Assignment3\PluginDevelopment\Plugin;
+ 
+class Cart
+{
+    // public function beforeAddProduct(\Magento\Checkout\Model\Cart $subject, 
+    // $productInfo,
+    // $requestInfo=null)
+    // {
+    //     $requestInfo['qty']=5;
+    //     return array($productInfo, $requestInfo);
+    // }
+    public function beforeAddProduct(\Magento\Checkout\Model\Cart $subject, 
+    $productInfo,
+    $requestInfo=null)
+    {
+        $requestInfo['price']=$requestInfo['price']+100;
+        return array($productInfo, $requestInfo);
+    }
+}
