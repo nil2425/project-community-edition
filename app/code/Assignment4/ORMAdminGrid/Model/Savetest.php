e13369e36c0236ed014b940698783cf535ef6c5f
nil2425 <nil2425@gmail.com>
2020-10-07 17:44:58 +0530

    Assignment 4 Updates - ORM Admin Grid

--- app/code/Assignment4/ORMAdminGrid/Model/Savetest.php
+++ app/code/Assignment4/ORMAdminGrid/Model/Savetest.php
@@ -1,13 +1,31 @@
 <?php
 namespace Assignment4\ORMAdminGrid\Model;
 use Magento\Framework\Model\AbstractModel;
-class Savetest extends AbstractModel
+class Savetest extends AbstractModel implements \Magento\Framework\DataObject\IdentityInterface
 {
     /**
      * Define resource model
      */
+
+    const CACHE_TAG = 'assignment4_contactus';
+
+    protected $_cacheTag = 'assignment4_contactus';
+    
+    protected $_eventPrefix = 'assignment4_contactus';
+
     protected function _construct()
     {
         $this->_init('Assignment4\ORMAdminGrid\Model\ResourceModel\Savetest');
+    }
+
+    public function getIdentities()
+    {
+    return [self::CACHE_TAG . '_' . $this->getId()];
+    }
+
+    public function getDefaultValues()
+    {
+    $values = [];
+    return $values;
     }
 }
